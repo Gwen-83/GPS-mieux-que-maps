9c85a9641f96ffd1cde00e4db6e414d797e859a3
Gwen-83 <gwenael.drouet@gmail.com>
2026-01-19 14:48:11 +0100

    variable

--- data/map.py
+++ data/map.py
@@ -3,7 +3,7 @@ V = "Voie rapide"
 N = "Nationale"
 D = "Départementale"
 
-{"Toulouse":{"Blagnac":[10,A], "Colomiers":[15, N],"Tournefeuille":[8, A]},
+maping = {"Toulouse":{"Blagnac":[10,A], "Colomiers":[15, N],"Tournefeuille":[8, A]},
 "Blagnac":{"Toulouse":[10, A], "Aussonne":[9,D], "Colomiers":[7, V]},
 "Colomiers":{"Toulouse":[15, N], "Blagnac":[7, V],"Tournefeuille":[5,D], "Aussonne":[12,N]},
 "Tournefeuille":{"Toulouse":[8,A], "Colomiers":[5, D]},
