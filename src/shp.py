cbefbb68e43b697d85084fc15a1a24e8a354590e
Gwen-83 <gwenael.drouet@gmail.com>
2026-01-19 17:26:59 +0100

    parsing suite

--- src/shp.py
+++ src/shp.py
@@ -1,20 +1,42 @@
 import geopandas as gpd
 from geopandas import sjoin
 
-# fichiers
 cities_file = r"C:\Users\Gwénaël\OneDrive\Bureau\ENAC\Programmation\projet_GPS\src\data\gis_osm_places_free_1.shp"
 roads_file  = r"C:\Users\Gwénaël\OneDrive\Bureau\ENAC\Programmation\projet_GPS\src\data\gis_osm_roads_free_1.shp"
 
-# lire les shapefiles
 cities = gpd.read_file(cities_file)
 roads  = gpd.read_file(roads_file)
 
-# filtrer pour simplifier
 cities = cities[cities['fclass'].isin(['city', 'town'])]
 roads  = roads[roads['fclass'].isin(['motorway', 'primary', 'secondary', 'tertiary'])]
 
-cities['buffer'] = cities.geometry.buffer(0.01)
+cities = cities.to_crs(epsg=2154)
+roads  = roads.to_crs(epsg=2154)
 
-roads_with_cities = sjoin(roads, cities[['name', 'buffer']], how='inner', predicate='intersects')
+cities['buffer_geom'] = cities.geometry.buffer(1000)
+cities_buffer = cities.set_geometry('buffer_geom')
 
-print(roads_with_cities[['name', 'fclass', 'geometry']].head())
+roads_with_cities = sjoin(roads, cities_buffer[['name', 'buffer_geom']], how='inner', predicate='intersects')
+
+roads_with_cities = roads_with_cities.rename(columns={'name_right': 'city_name', 'name_left': 'road_name'})
+
+roads_with_cities = roads_with_cities.to_crs(epsg=4326)
+
+cities_routes = roads_with_cities.groupby('city_name').agg({
+    'road_name': list,
+    'fclass': list,
+    'geometry': 'first',
+    'maxspeed': list
+}).reset_index()
+
+print(cities_routes.head())
+
+# convertir les colonnes listes en chaînes séparées par des virgules
+cities_routes['road_name'] = cities_routes['road_name'].apply(lambda x: ', '.join(map(str, x)))
+cities_routes['fclass']    = cities_routes['fclass'].apply(lambda x: ', '.join(map(str, x)))
+cities_routes['maxspeed']  = cities_routes['maxspeed'].apply(lambda x: ', '.join(map(str, x)))
+
+# exporter en CSV
+cities_routes.to_csv(r"C:\Users\Gwénaël\OneDrive\Bureau\ENAC\Programmation\projet_GPS\cities_routes.csv", index=False)
+
+print("CSV exporté avec succès !")
