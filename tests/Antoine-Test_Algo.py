46481f6e687fe846c31b6797dab266df1f82732c
Antoine Hardy <antoine.hardy.149@gmail.com>
2026-01-22 19:11:53 +0100

    Ajout fichier dev et test Antoine

--- tests/Antoine-Test_Algo.py
+++ tests/Antoine-Test_Algo.py
@@ -1,7 +1,3 @@
-import sys
-import os
-sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '../src')))
-
 from map import maping
 from localisation import localisation_ville
 import math
