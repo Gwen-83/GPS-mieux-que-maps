b1f8a2efcb7511434d1ba9d11dad292c5248d0bc
Gwen-83 <158830850+Gwen-83@users.noreply.github.com>
2026-02-17 17:26:34 +0100

    Update localisation.py

--- src/localisation.py
+++ src/localisation.py
@@ -1,8 +1,19 @@
 import json
 import os
 
-CURRENT_DIR = os.path.dirname(os.path.abspath(__file__))  # src/
+# map.py est dans src/
+CURRENT_DIR = os.path.dirname(os.path.abspath(__file__)) 
+
+# On descend dans le dossier visualisation pour trouver les fichiers
+# Chemin : src/visualisation/dico_final.json
 PATH_LOCALISATION = os.path.join(CURRENT_DIR, "visualisation", "coords_villes.json")
 
-with open(PATH_LOCALISATION, encoding="utf-8") as f:
-    localisation_ville = json.load(f)
+print(f"Tentative de chargement du dico depuis : {PATH_LOCALISATION}")
+
+if os.path.exists(PATH_LOCALISATION):
+    with open(PATH_LOCALISATION, encoding="utf-8") as f:
+        localisation_ville = json.load(f)
+    print("✅ maping chargé avec succès.")
+else:
+    print(f"❌ ERREUR : Fichier introuvable à l'emplacement : {PATH_LOCALISATION}")
+    maping = {}
